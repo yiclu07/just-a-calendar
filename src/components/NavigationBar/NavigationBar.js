f174ca5469fa31e462e2479aa26265eb7f163b38
i-love-sana <yi.c.lu07@gmail.com>
2021-11-17 13:22:26 -0500

    Add month change buttons

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,11 +1,31 @@
 import styles from './NavigationBar.module.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
-const NavigationBar = () => {
+library.add(faChevronLeft, faChevronRight);
+
+function NavigationBar() {
   return (
-    <div>
-
+    <div className={styles.NavigationBar}>
+      <button
+        className={styles.navButton}
+        onClick={/* go to previous month */}
+      >
+        <FontAwesomeIcon icon="chevron-left" />
+      </button>
+      <div className={styles.dateHeading}>
+        <h1>{/* Month */`November`}</h1>
+        <p>{new Date().toDateString()}</p>
+      </div>
+      <button
+        className={styles.navButton}
+        onClick={/* go to next month */}
+      >
+        <FontAwesomeIcon icon="chevron-right" />
+      </button>
     </div>
-  )
+  );
 }
 
 export default NavigationBar;
