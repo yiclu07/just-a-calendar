7acaf137df74da9347681226ef5850ed0b5919f8
i-love-sana <yi.c.lu07@gmail.com>
2021-11-17 14:21:51 -0500

    Function to get dates from next month

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-import { getDay, getDaysInMonth } from "date-fns";
+import { startOfMonth, getDay, getDaysInMonth, lastDayOfMonth } from "date-fns";
 
 /* 
   getPrevMonthDates(date) takes a Date object as an argument; it returns an array that contains the remaining dates of the previous month that could appear on a calendar page for the month of the Date object
@@ -10,7 +10,7 @@ import { getDay, getDaysInMonth } from "date-fns";
     prevMonthDates would equal: [
       27, 28, 29, 30, 31
     ]
-    because these dates from December 2020 could appear on a calendar page for January 2021
+    because these dates from December 2020 could appear on a calendar page for January 2021 in the first week
 
 */
 export const getPreviousMonthDates = (date) => {
@@ -45,4 +45,26 @@ export const getCurrentMonthDates = (date) => {
   }
 
   return currentMonthDatesArr;
+}
+
+/* 
+  getNextMonthDates(date) takes a Date object as an argument; it returns an array that contains the first dates of the following month that could appear on a calendar page for the month of the Date object
+
+  EXAMPLE:
+    const nextMonthDates = getNextMonthDates(new Date(2021, 0, 1));
+
+    nextMonthDates would equal: [
+      1, 2, 3, 4, 5, 6
+    ]
+    because these dates from February 2021 could appear on a calendar page for January 2021 in the final week
+*/
+export const getNextMonthDates = (date) => {
+  const nextMonthDatesArr = [];
+  const lastDayOfCurrentMonth = getDay(lastDayOfMonth(date));
+
+  for (let i = 1; i <= 6 - lastDayOfCurrentMonth; i++) {
+    nextMonthDatesArr.push(i);
+  }
+
+  return nextMonthDatesArr;
 }
