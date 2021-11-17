1d32ccf9ae84af1ae9a34477115abf8cb89c89d4
i-love-sana <yi.c.lu07@gmail.com>
2021-11-17 00:02:16 -0500

    Render days of week

--- src/components/Weekdays/Weekdays.js
+++ src/components/Weekdays/Weekdays.js
@@ -2,8 +2,14 @@ import styles from './Weekdays.module.css';
 
 const Weekdays = () => {
   return (
-    <div>
-
+    <div className={styles.Weekdays}>
+      <div className={styles.weekday}>Sun</div>
+      <div className={styles.weekday}>Mon</div>
+      <div className={styles.weekday}>Tues</div>
+      <div className={styles.weekday}>Wed</div>
+      <div className={styles.weekday}>Thurs</div>
+      <div className={styles.weekday}>Fri</div>
+      <div className={styles.weekday}>Sat</div>
     </div>
   )
 }
