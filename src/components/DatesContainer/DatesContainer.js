ab70e860ba153588b7f2e15c145368eb8c6bbc58
i-love-sana <yi.c.lu07@gmail.com>
2021-11-19 01:58:22 -0500

    Render DateTile components

--- src/components/DatesContainer/DatesContainer.js
+++ src/components/DatesContainer/DatesContainer.js
@@ -1,8 +1,51 @@
 import styles from './DatesContainer.module.css';
+import DateTile from '../DateTile/DateTile.js';
+import { getCurrentMonthDates, getNextMonthDates, getPreviousMonthDates } from '../../utils';
+import { getMonth, getYear, isToday } from 'date-fns';
 
-const DatesContainer = () => {
+const DatesContainer = ({ date }) => {
+  const prevMonthDates = getPreviousMonthDates(date);
+  const currentMonthDates = getCurrentMonthDates(date);
+  const nextMonthDates = getNextMonthDates(date);
+
   return (
-    <div></div>
+    <div>
+      {prevMonthDates.map((calendarDate, index) => {
+        return (
+          <DateTile
+            key={index}
+            dateNumber={calendarDate}
+            isToday={isToday(new Date(getYear(date), getMonth(date), calendarDate))}
+            isPrevMonth={true}
+            isNextMonth={false}
+          />
+        )
+      })}
+
+      {currentMonthDates.map((calendarDate, index) => {
+        return (
+          <DateTile
+            key={index}
+            dateNumber={calendarDate}
+            isToday={isToday(new Date(getYear(date), getMonth(date), calendarDate))}
+            isPrevMonth={false}
+            isNextMonth={false}
+          />
+        )
+      })}
+
+      {nextMonthDates.map((calendarDate, index) => {
+        return (
+          <DateTile
+            key={index}
+            dateNumber={calendarDate}
+            isToday={isToday(new Date(getYear(date), getMonth(date), calendarDate))}
+            isPrevMonth={false}
+            isNextMonth={true}
+          />
+        )
+      })}
+    </div>
   );
 }
 
