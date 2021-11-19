725892b96813e6886a5bb9ff57262dbc1249cffd
i-love-sana <yi.c.lu07@gmail.com>
2021-11-19 02:01:20 -0500

    Import and render calendar components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import './App.css';
+import { useState } from 'react';
+import DatesContainer from './components/DatesContainer/DatesContainer';
+import NavigationBar from './components/NavigationBar/NavigationBar';
+import Weekdays from './components/Weekdays/Weekdays';
 
 function App() {
+  const [date, setDate] = useState(new Date());
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calendar-container">
+      <div className="calendar">
+        <NavigationBar date={date} onDateChange={setDate}/>
+        <Weekdays />
+        <DatesContainer date={date}/>
+      </div>
     </div>
   );
 }
